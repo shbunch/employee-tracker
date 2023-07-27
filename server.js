e60ce5aadedd28622b50edf296e3792991146c40
shbunch <shawnbunch@gmail.com>
2023-07-27 16:59:08 -0500

    Added some helper/commented code and created preliminary SQL databases

---
+++ server.js
@@ -0,0 +1,13 @@
+// View All Departments
+// View All Roles
+// View All Employees
+// Add a Department
+// Add a Role
+// Add an Employee
+// Update an Employee Role
+
+Departments
+1 - Sales
+2 - Engineering
+3 - Finance
+4 - Legal
